e2680419ed7a8caed2d9d927e86acf9510d0b06b
AdamBachorz <adam.bachorz1702@gmail.com>
2022-06-30 18:33:54 +0200

    Add AutoMapper config to Web

---
+++ BachorzLibrary.Web/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using BachorzLibrary.Web.Config;
+
+namespace BachorzLibrary.Web.Extensions
+{
+    public static  class ServiceCollectionExtensions
+    {
+        public static void AddAutoMapperFixture(this IServiceCollection service, MapperConfigurationExpression cfg)
+        {
+            service.AddSingleton<IMapper>(service => {
+                var config = new MapperConfiguration(cfg);
+                return new Mapper(config);
+            });
+        }
+    }
+}
