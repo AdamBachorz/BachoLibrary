341ef39bba518de2138d12ab5d06d1c2fa847f38
AdamBachorz <adam.bachorz1702@gmail.com>
2022-07-02 17:30:54 +0200

    Add Performance Checker

---
+++ BachoLibrary.Tests/TestField.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using BachorzLibrary.Common;
+using BachorzLibrary.Common.Extensions;
+using BachorzLibrary.Common.Tools;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BachoLibrary.Tests
+{
+    [TestFixture]
+    public class TestField
+    {
+        [Test]
+        [Explicit]
+        public void Test()
+        {
+            Action action = () => Thread.Sleep(2000);
+
+            var performance = new PerformanceChecker(action);
+            performance.StartPerformanceAsync();
+
+            Thread.Sleep(5000);
+        }
+
+        [Test]
+        [Explicit]
+        public async Task TestAsync()
+        {
+            
+        }
+    }
+}
---
+++ BachorzLibrary.Common/Tools/PerformanceChecker.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BachorzLibrary.Common.Tools
+{
+    public class PerformanceChecker
+    {
+        private readonly Stopwatch _stopwatch;
+        private Thread _thread;
+
+        public Action Action { get; set; }
+
+        public PerformanceChecker(Action action)
+        {
+            Action = action;
+            _stopwatch = new Stopwatch();
+        }
+        public void StartPerformance()
+        {
+            _stopwatch.Reset();
+            _stopwatch.Start();
+            Action();
+            _stopwatch.Stop();
+        }
+
+        public void StartPerformanceAsync()
+        {
+            _thread = new Thread(StartPerformance);
+            _thread.Start();
+        }       
+
+        public TimeSpan TimeSpan => _stopwatch.Elapsed;
+        public bool NotMeasured => TimeSpan == TimeSpan.Zero;
+
+        public string Report
+        {
+            get
+            {
+                if (_thread?.ThreadState == System.Threading.ThreadState.Running)
+                {
+                    return "Performance check is running. Wait until it's done.";
+                }
+                if (NotMeasured)
+                {
+                    return "Performance check didn't start yet";
+                }
+
+                var sb = new StringBuilder();
+                sb.AppendLine($"Procedure: {Action.Method.Name}");
+                sb.AppendLine($"Hours: {TimeSpan.Hours}");
+                sb.AppendLine($"Minutes {TimeSpan.Minutes}");
+                sb.AppendLine($"Seconds {TimeSpan.Seconds}");
+                sb.AppendLine($"Milliseconds {TimeSpan.Milliseconds}");
+
+                return sb.ToString();
+            }
+        }
+    }
+}
