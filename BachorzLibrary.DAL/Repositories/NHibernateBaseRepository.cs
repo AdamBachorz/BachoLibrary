36dedae5ca7cc5bdf192c6fb505d34a78f57e28c
AdamBachorz <adam.bachorz1702@gmail.com>
2022-07-06 04:41:26 +0200

    Create NHibernateBaseRepository.cs

---
+++ BachorzLibrary.DAL/Repositories/NHibernateBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using BachorzLibrary.DAL.DAO;
+using BachorzLibrary.DAL.NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BachorzLibrary.DAL.Repositories
+{
+    public class NHibernateBaseRepository<E> : NHibernateBaseDao<E> where E : Entity
+    {
+        public NHibernateBaseRepository(INHibernateHelper nHibernateHelper) : base(nHibernateHelper)
+        {
+        }
+    }
+}
