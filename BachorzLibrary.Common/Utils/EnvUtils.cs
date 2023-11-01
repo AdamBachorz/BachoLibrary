71082cef100b4f9baec1024c15a0885fbfe0b2a9
AdamBachorz <adam.bachorz1702@gmail.com>
2023-11-01 21:04:17 +0100

    Add DbContextUtils and EnvUtils

---
+++ BachorzLibrary.Common/Utils/EnvUtils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BachorzLibrary.Common.Utils
+{
+    public static class EnvUtils
+    {
+        public static void PerformDependingOnEnvironment(Action onDev, Action onProd)
+        {
+#if DEBUG
+            onDev();
+#else
+            onProd();
+#endif
+        }
+        
+        public static async Task PerformDependingOnEnvironmentAsync(Func<Task> onDev, Func<Task> onProd)
+        {
+#if DEBUG
+            await onDev();
+#else
+            await onProd();
+#endif
+        }
+        
+        public static V GetValueDependingOnEnvironment<V>(V devValue, V prodValue)
+        {
+#if DEBUG
+            return devValue;
+#else
+            return prodValue;
+#endif
+        }
+    }
+}
---
+++ BachorzLibrary.DAL.DotNetSix/Utils/DbContextUtils.cs
@@ -0,0 +1,32 @@
+﻿using BachorzLibrary.Common.Utils;
+using BachorzLibrary.DAL.DotNetSix.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace BachorzLibrary.DAL.DotNetSix.Utils
+{
+    public static class DbContextUtils
+    {
+        public static void ExplicitConfig(DbContextOptionsBuilder optionsBuilder, string configFile)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                if (!File.Exists(configFile))
+                {
+                    throw new FileLoadException("Config file not found", 
+                        EnvUtils.GetValueDependingOnEnvironment("DEV", "PROD"));
+                }
+
+                var config = JsonConvert.DeserializeObject<EFCCustomConfig>(File.ReadAllText(configFile));
+                if (config.IsProduction)
+                {
+                    optionsBuilder.UseNpgsql(config.ConnectionString);
+                }
+                else
+                {
+                    optionsBuilder.UseSqlite(config.ConnectionString);
+                }
+            }
+        }
+    }
+}
