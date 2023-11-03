a21956ddf27cb590b26558e4735ea25132c58d59
AdamBachorz <adam.bachorz1702@gmail.com>
2023-11-03 15:02:33 +0100

    Update ServiceCollectionExtensions

---
+++ BachorzLibrary.Common/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using BachorzLibrary.Common.Tools.Email;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace BachorzLibrary.Common.Extensions
+{
+    public static  class ServiceCollectionExtensions
+    {
+        public static void AddObjectMappingConfiguration(this IServiceCollection service, MapperConfigurationExpression cfg)
+        {
+            service.AddSingleton<IMapper>(service => {
+                var config = new MapperConfiguration(cfg);
+                return new Mapper(config);
+            });
+        }
+
+        public static void AddEmailSenderService(this IServiceCollection services, Action<EmailSenderSettings> settingsInvoker)
+        {
+            var settings = new EmailSenderSettings();
+            settingsInvoker(settings);
+            var emailSenderService = new EmailSender(settings);
+            services.AddSingleton(emailSenderService);
+        }
+    }
+}
---
+++ BachorzLibrary.DAL/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using BachorzLibrary.DAL.NHibernate;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BachorzLibrary.Common.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddNHibernateORM(this IServiceCollection service, IFluentNHibernateCustomConfig fluentNHibernateCustomConfig)
+        {
+            service.AddSingleton(fluentNHibernateCustomConfig);
+            service.AddScoped<INHibernateHelper, NHibernateHelper>();
+        }
+    }
+}
