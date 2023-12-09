b2e336eefa20981bc49472b3464f492ee74ff578
AdamBachorz <adam.bachorz1702@gmail.com>
2023-12-09 13:21:56 +0100

    Add new DataBase switch case to DbContextUtils ExplicitConfig function

--- BachorzLibrary.DAL.DotNetSix/Utils/DbContextUtils.cs
+++ BachorzLibrary.DAL.DotNetSix/Utils/DbContextUtils.cs
@@ -21,16 +21,17 @@ namespace BachorzLibrary.DAL.DotNetSix.Utils
 
                 switch (config.DataBase)
                 {
+                    case DataBase.MSSQL:
+                        optionsBuilder.UseSqlServer(config.ConnectionString);
+                        break;
                     case DataBase.PostgreSQL:
                         optionsBuilder.UseNpgsql(config.ConnectionString);
                         break;
                     case DataBase.Sqlite:
                         optionsBuilder.UseSqlite(config.ConnectionString);
                         break;
-                    case DataBase.MSSQL:
-                        optionsBuilder.UseSqlServer(config.ConnectionString);
-                        break;
                     case DataBase.MySQL:
+                    case DataBase.Oracle:
                     default:
                         throw new NotSupportedException($"DataBase {config.DataBase} not supported.");     
                 }
--- BachorzLibrary.DAL/DAL_pack_local.ps1
+++ BachorzLibrary.DAL/DAL_pack_local.ps1
@@ -1,4 +1,4 @@
 ﻿# fast local release of a nuget
 Get-ChildItem .\bin\Debug\*.nupkg | Remove-Item
-dotnet pack /p:Version=0.0.5 -c Debug
+dotnet pack /p:Version=0.0.6 -c Debug
 Move-Item .\bin\Debug\*.nupkg c:\nuget\ -Force
