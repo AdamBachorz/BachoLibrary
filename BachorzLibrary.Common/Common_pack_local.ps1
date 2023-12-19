12115196d8502907081cc65f2c69793019b62e07
AdamBachorz <adam.bachorz1702@gmail.com>
2023-12-19 17:23:46 +0100

    Change EnvUtils determination of dev/prod

--- BachorzLibrary.Common/Common_pack_local.ps1
+++ BachorzLibrary.Common/Common_pack_local.ps1
@@ -1,4 +1,4 @@
 ﻿# fast local release of a nuget
 Get-ChildItem .\bin\Debug\*.nupkg | Remove-Item
-dotnet pack /p:Version=0.0.10 -c Debug
+dotnet pack /p:Version=0.0.11 -c Debug
 Move-Item .\bin\Debug\*.nupkg c:\nuget\ -Force
--- BachorzLibrary.Common/Utils/EnvUtils.cs
+++ BachorzLibrary.Common/Utils/EnvUtils.cs
@@ -1,39 +1,39 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace BachorzLibrary.Common.Utils
 {
     public static class EnvUtils
     {
-        public static bool IsDev() => GetValueDependingOnEnvironment(true, false);
+        public static bool IsDev() => Debugger.IsAttached;
 
         public static bool IsProduction() => !IsDev();
 
         public static void PerformDependingOnEnvironment(Action onDev, Action onProd)
         {
-#if DEBUG
-            onDev();
-#else
-            onProd();
-#endif
+            if (IsDev())
+            {
+                onDev(); 
+            }
+            else 
+            {
+                onProd();
+            }
         }
         
         public static async Task PerformDependingOnEnvironmentAsync(Func<Task> onDev, Func<Task> onProd)
         {
-#if DEBUG
-            await onDev();
-#else
-            await onProd();
-#endif
+            if (IsDev())
+            {
+                await onDev();
+            }
+            else
+            {
+                await onProd();
+            }
         }
         
-        public static V GetValueDependingOnEnvironment<V>(V devValue, V prodValue)
-        {
-#if DEBUG
-            return devValue;
-#else
-            return prodValue;
-#endif
-        }
+        public static V GetValueDependingOnEnvironment<V>(V devValue, V prodValue) => IsDev() ? devValue : prodValue;
     }
 }
